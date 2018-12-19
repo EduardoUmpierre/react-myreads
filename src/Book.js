241bd1639d940417dae00540a873f4f3d86b0cb3
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-12-19 14:47:12 -0200

    Stateless component updated to pure component

--- src/Book.js
+++ src/Book.js
@@ -1,63 +1,67 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-const Book = ({
-	id,
-	title,
-	authors,
-	image,
-	onBookshelfChange,
-	currentBookshelf
-}) => {
-	return (
-		<div className="book">
-			<div className="book-top">
-				<div
-					className="book-cover"
-					style={{
-						width: 128,
-						height: 193,
-						backgroundImage: `url("${image}")`
-					}}
-				/>
-				<div className="book-shelf-changer">
-					<select
-						onChange={event =>
-							onBookshelfChange(event, {
-								id,
-								title,
-								authors,
-								image
-							})
-						}
-						value={currentBookshelf ? currentBookshelf : 'none'}
-					>
-						<option value="move" disabled>
-							Move to...
-						</option>
-						<option value="currentlyReading">
-							Currently Reading
-						</option>
-						<option value="wantToRead">Want to Read</option>
-						<option value="read">Read</option>
-						<option value="none">None</option>
-					</select>
-				</div>
-			</div>
-			<div className="book-title">{title}</div>
+class Book extends PureComponent {
+	render() {
+		const {
+			id,
+			title,
+			authors,
+			image,
+			onBookshelfChange,
+			currentBookshelf
+		} = this.props
 
-			{authors && (
-				<div className="book-authors">
-					{authors.map((author, index) => (
-						<div key={index}>
-							{author}
-							{index < authors.length - 1 && ', '}
-						</div>
-					))}
+		return (
+			<div className="book">
+				<div className="book-top">
+					<div
+						className="book-cover"
+						style={{
+							width: 128,
+							height: 193,
+							backgroundImage: `url("${image}")`
+						}}
+					/>
+					<div className="book-shelf-changer">
+						<select
+							onChange={event =>
+								onBookshelfChange(event, {
+									id,
+									title,
+									authors,
+									image
+								})
+							}
+							value={currentBookshelf ? currentBookshelf : 'none'}
+						>
+							<option value="move" disabled>
+								Move to...
+							</option>
+							<option value="currentlyReading">
+								Currently Reading
+							</option>
+							<option value="wantToRead">Want to Read</option>
+							<option value="read">Read</option>
+							<option value="none">None</option>
+						</select>
+					</div>
 				</div>
-			)}
-		</div>
-	)
+				<div className="book-title">{title}</div>
+
+				{authors && (
+					<div className="book-authors">
+						{authors.map((author, index) => (
+							<div key={index}>
+								{author}
+								{index < authors.length - 1 && ', '}
+							</div>
+						))}
+					</div>
+				)}
+			</div>
+		)
+	}
 }
 
 Book.propTypes = {
